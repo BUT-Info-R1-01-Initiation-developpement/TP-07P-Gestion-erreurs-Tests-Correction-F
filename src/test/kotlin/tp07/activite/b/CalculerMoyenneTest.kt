e72a1a87c8a8687aaf02cc9cbe2a82ba98891681
Franck Silvestre <franck.silvestre@gmail.com>
2025-10-20 16:54:42 +0200

    fix #2 Tests de calcul de moyenne commenté.

--- src/test/kotlin/tp07/activite/b/CalculerMoyenneTest.kt
+++ src/test/kotlin/tp07/activite/b/CalculerMoyenneTest.kt
@@ -8,24 +8,26 @@ class CalculerMoyenneTest {
 
     // Regarde les noms des fonctions : ils contiennent des espaces.
     // Mais, as-tu repéré dans la syntaxe Kotlin, à quelle condition cela était possible ?
-    // ...
+    // C'est possible uniquement si le nom de la fonction est encadré par ``.
     // Attention : cette approche n'est recommandée que pour les fonctions de tests car elle facilite
     // La lecture des résultats des tests.
 
     // Dans les tests ci-dessous, qu'est-ce qui correspond :
     // - aux tests du ou des cas nominaux ?
-    // ...
+    // `test moyenne avec notes valides`()
+    // toutes les notes à 20, ou à 0
     // - aux tests du ou des cas limites ?
-    // ...
+    // `test moyenne avec une seule note`()
     // - aux tests du ou des cas d'erreur ?
-    // ...
+    // - tableau vide
+    // - cas des notes négatives ou supérieur 0
 
     // Lance les tests avec la mesure de la couverture du code par les tests.
     // Est-ce que la fonction calculerMoyenne() est couverte à 100% ?
-    // ...
+    //
     // Si elle n'est pas couverte à 100%, dans ce cas, peut-on considérer que la non couverture à 100%
     // est acceptable ?
-    // ...
+    // Oui, il est acceptable de pas avoir 100% car, 100% révèlerait un bug dans le programme
     // ...
 
     @Test
